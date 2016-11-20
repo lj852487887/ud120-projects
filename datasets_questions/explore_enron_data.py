411310b34b63fa8a5535be09149ed989dec15489
李骏 <lj852487887@gmail.com>
2016-11-20 15:58:08 +0800

    add enron data explore

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -18,5 +18,25 @@
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+lastname = "Skilling".upper()
+firstname = "Jeffrey".upper()
+print enron_data[lastname+" "+firstname+" K"]
+
+salaryCount = 0
+emailCount = 0
+count = 0
+for i in enron_data:
+    if enron_data[i]['poi']==True:
+        count+=1
+
+print count
+
+nanCount = 0
+for i in enron_data:
+    if enron_data[i]["total_payments"]=='NaN':
+        nanCount+=1
+print float(nanCount)/len(enron_data)
+
+
 
 
