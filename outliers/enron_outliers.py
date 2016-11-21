4a94d08272b2ffb2d55d2e6740ce85683652738d
李骏 <lj852487887@gmail.com>
2016-11-21 14:14:22 +0800

    add enron outliers

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,11 +9,28 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop('TOTAL', 0)
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
+
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
+
+import pandas as pd
+df = pd.DataFrame(data_dict)
+df.loc['salary',:] = pd.to_numeric(df.loc['salary',:], errors='coerce')
+df.loc['bonus',:] = pd.to_numeric(df.loc['bonus',:], errors='coerce')
+
+print df.loc['salary',:].idxmax(axis=1)
+print [name for name in df.columns if df.loc['salary', name] > 10**6 and df.loc['bonus',name] > 5*10**6]
 
 
 
--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,15 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
+    error = list( (net_worths - predictions)**2 )
 
+    cleaned_data = zip(ages, net_worths, error)
+    cleaned_data = sorted(cleaned_data, key = lambda tup: tup[2])
+    cleaned_data = cleaned_data[:80]
     
     return cleaned_data
 
